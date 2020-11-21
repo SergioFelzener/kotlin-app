302f29b14b9f141445338f187e5a2a4e6bdd9c20
Sergio Felzener <felzenerdj@gmail.com>
2020-11-20 21:41:02 -0300

    Iniciando Reddit com Auth Firebase

---
+++ Reddit/app/src/main/java/br/senac/redditcover/MainActivity.kt
@@ -0,0 +1,57 @@
+package br.senac.redditcover
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // se estiver usuário logado
+        if(getCurrentUser() == null ) {
+
+            val providers
+                    = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+            val i = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build()
+
+            startActivityForResult(i, 0)
+
+        }else {
+
+            Toast.makeText(this, "Autenticado", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Autenticado", Toast.LENGTH_LONG).show()
+            }
+            else {
+                finishAffinity()
+            }
+        }
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+
+        //instancia firebase Auth
+        val auth = FirebaseAuth.getInstance()
+        return auth.currentUser
+
+    }
+
+}
